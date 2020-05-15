0ce2503f011341b8df7f51930ad67d9edaeb1e52
RenatDoroshenko <renat.doroshenko@sitecore.com>
2020-05-15 08:44:36 +0300

    first commit

---
+++ src/Sitecore.Support.389445/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.389445")]
+[assembly: AssemblyProduct("Sitecore.Support.389445")]
+[assembly: ComVisible(false)]
