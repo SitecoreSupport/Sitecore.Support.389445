2b5910c0ef04c734a11e85d362929a90e8e121f7
mao <mao@sitecore.net>
2020-08-01 16:25:01 +0300

    Initial commit

---
+++ src/Sitecore.Support.389445/Pipelines/Blocks/FindEntityInMemoryCacheBlock.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Extensions.Logging;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Core.Caching;
+using Sitecore.Framework.Pipelines;
+using System;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+
+namespace Sitecore.Support.Commerce.Core
+{
+    public class FindEntityInMemoryCacheBlock : PipelineBlock<FindEntityArgument, FindEntityArgument, CommercePipelineExecutionContext>
+    {
+        private readonly IGetEnvironmentCachePipeline _cachePipeline;
+
+        public FindEntityInMemoryCacheBlock(IGetEnvironmentCachePipeline cachePipeline)
+        : base((string)null)
+        {
+            _cachePipeline = cachePipeline;
+        }
+
+        public override async Task<FindEntityArgument> Run(FindEntityArgument arg, CommercePipelineExecutionContext context)
+        {
+            var entityCachePolicy = EntityMemoryCachingPolicy.GetCachePolicy(context.CommerceContext, arg.EntityType);
+            if (!entityCachePolicy.AllowCaching)
+            {
+                // Skip caching if not enabled for this entity
+                return arg;
+            }
+
+            var itemKey = arg.EntityId;
+
+            var requestedVersion = context.GetModel<RequestedEntityVersion>(x => x.EntityId.Equals(arg.EntityId, StringComparison.OrdinalIgnoreCase));
+            if (arg.EntityVersion.HasValue)
+            {
+                itemKey += $"-{arg.EntityVersion}";
+                context.AddModel(new CacheRequest(arg.EntityId, arg.EntityVersion));
+            }
+            else if (requestedVersion != null)
+            {
+                itemKey += $"-{requestedVersion.EntityVersion}";
+                context.AddModel(new CacheRequest(arg.EntityId, requestedVersion.EntityVersion));
+            }
+            else
+            {
+                context.AddModel(new CacheRequest(arg.EntityId));
+            }
+
+            var typeName = arg.EntityType.Name;
+
+            context.Logger.LogDebug($"{this.Name} looking for {itemKey}");
+
+            var cache = await this._cachePipeline.Run(new EnvironmentCacheArgument { CacheName = entityCachePolicy.CacheName }, context);
+
+            if (entityCachePolicy.CacheAsEntity)
+            {
+                var data = await cache.Get(itemKey) as CommerceEntity;
+                if (data == null)
+                {
+                    context.Logger.LogInformation($"Core.MemCache.CE.Miss.{typeName}: ItemKey={itemKey}");
+                }
+                else
+                {
+                    if (!context.HasPolicy<IgnorePublishedPolicy>() && !data.Published)
+                    {
+                        context.Logger.LogDebug($"Core.MemCache.CE.NotPublished.{typeName}: ItemKey={itemKey}");
+                        return arg;
+                    }
+
+                    context.Logger.LogDebug($"Core.MemCache.CE.Hit.{typeName}: ItemKey={itemKey}");
+                    context.AddModel(new CacheHit(itemKey));
+
+                    context.CommerceContext.AddObject(new FoundEntity
+                    {
+                        EntityId = arg.EntityId,
+                        Entity = data,
+                        EntityVersion = data.EntityVersion,
+                        FoundInCache = true,
+                        CachedAsEntity = true
+                    });
+                    arg.Entity = data;
+                }
+            }
+            else
+            {
+                var data = await cache.Get(itemKey) as string;
+                if (data == null)
+                {
+                    context.Logger.LogInformation($"Core.MemCache.S.Miss.{typeName}: ItemKey={itemKey}");
+                }
+                else
+                {
+                    context.Logger.LogDebug($"Core.MemCache.S.Hit.{typeName}: ItemKey={itemKey}");
+                    context.AddModel(new CacheHit(itemKey));
+                    context.CommerceContext.AddObject(new FoundEntity
+                    {
+                        EntityId = arg.EntityId,
+                        SerializedEntity = data,
+                        EntityVersion = arg.EntityVersion ?? 1,
+                        FoundInCache = true,
+                        CachedAsEntity = false
+                    });
+                    arg.SerializedEntity = data;
+                }
+            }
+
+            return arg;
+        }
+    }
+}
---
+++ src/Sitecore.Support.389445/Pipelines/Blocks/StoreCommerceEntityInMemoryCacheBlock.cs
@@ -0,0 +1,110 @@
+﻿using Force.DeepCloner;
+using Microsoft.Extensions.Logging;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Core.Caching;
+using Sitecore.Commerce.Core.Commands;
+using Sitecore.Framework.Caching;
+using Sitecore.Framework.Pipelines;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sitecore.Support.Commerce.Core
+{
+    public class StoreCommerceEntityInMemoryCacheBlock : PipelineBlock<CommerceEntity, CommerceEntity, CommercePipelineExecutionContext>
+    {
+        private readonly IGetEnvironmentCachePipeline _cachePipeline;
+
+        private readonly EntitySerializerCommand _entitySerializerCommand;
+
+        public StoreCommerceEntityInMemoryCacheBlock(IGetEnvironmentCachePipeline cachePipeline, EntitySerializerCommand entitySerializerCommand)
+            : base((string)null)
+        {
+            _cachePipeline = cachePipeline;
+            _entitySerializerCommand = entitySerializerCommand;
+        }
+
+        public override async Task<CommerceEntity> Run(CommerceEntity arg, CommercePipelineExecutionContext context)
+        {
+            if (arg == null)
+            {
+                // Do not cache null entries
+                return null;
+            }
+
+            var foundEntity = context.CommerceContext.GetObjects<FoundEntity>().FirstOrDefault(p => p.EntityId.Equals(arg.Id, StringComparison.OrdinalIgnoreCase));
+
+            // Setup the item 
+            var itemKey = arg.Id;
+
+            var cacheRequest = context.GetModel<CacheRequest>(x => x.EntityId.Equals(arg.Id, StringComparison.OrdinalIgnoreCase));
+            if (cacheRequest != null && cacheRequest.Version.HasValue)
+            {
+                itemKey += $"-{cacheRequest.Version}";
+            }
+            else if (cacheRequest == null)
+            {
+                itemKey += $"-{arg.EntityVersion}";
+            }
+
+            var cacheHits = context.GetModels<CacheHit>();
+            for (var index = 0; index < cacheHits.Count; index += 1)
+            {
+                if (cacheHits[index].ItemKey.Equals(itemKey, StringComparison.OrdinalIgnoreCase))
+                {
+                    // CacheHit model existence means that this item was already retrieved from the cache so no need to Add it back
+                    return arg;
+                }
+            }
+
+            var typeName = arg.GetType().Name;
+            var entityCachePolicy = EntityMemoryCachingPolicy.GetCachePolicy(context.CommerceContext, arg.GetType());
+            if (!entityCachePolicy.AllowCaching)
+            {
+                if (context.GetPolicy<CacheLoggingPolicy>().LogSkipCache)
+                {
+                    context.Logger.LogInformation($"Core.MemCache.CE.SkipCache.NA.{typeName}: ItemKey={itemKey}");
+                }
+
+                return arg;
+            }
+
+            var entryOptions = entityCachePolicy.GetCacheEntryOptions();
+            var cache = await this._cachePipeline.Run(new EnvironmentCacheArgument { CacheName = entityCachePolicy.CacheName }, context.CommerceContext.GetPipelineContextOptions());
+            if (entityCachePolicy.CacheAsEntity)
+            {
+                await cache.Set(itemKey, new Cachable<CommerceEntity>(arg, 1), entryOptions);
+
+                context.Logger.LogInformation($"Core.MemCache.CE.Add.{typeName}: ItemKey={itemKey} cache:{cache.Name}");
+
+                return arg;
+            }
+            else
+            {
+                if (foundEntity == null)
+                {
+                    context.Logger.LogInformation($"Core.MemCache.CS.Add.NoFoundEntity-{itemKey} cache:{cache.Name}");
+                }
+                else
+                {
+                    if (foundEntity.FoundInCache)
+                    {
+                        context.Logger.LogDebug($"Core.MemCache.CS.SkipCache.AlreadyInCache-{itemKey} cache:{cache.Name}");
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(foundEntity.SerializedEntity))
+                        {
+                            foundEntity.SerializedEntity = await this._entitySerializerCommand.SerializeEntity(context.CommerceContext, foundEntity.Entity as CommerceEntity);
+                        }
+
+                        context.Logger.LogInformation($"Core.MemCache.CS.Add-{itemKey} cache:{cache.Name}");
+                        await cache.Set(itemKey, new Cachable<string>(foundEntity.SerializedEntity, 1), entryOptions);
+                    }
+                }
+
+                return arg;
+            }
+        }
+    }
+}
